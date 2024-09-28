ff602d950debdb151b58e05d720e37d4199743f0
andrei172004 <paulandrei.roncesvalles@sdca.edu.ph>
2024-09-28 16:04:52 +0800

    Add files via upload

---
+++ output.php
@@ -0,0 +1,36 @@
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $email              = $_POST["email"];
+        $username           = $_POST["username"];
+        $password           = $_POST["password"];
+        $confirm_password   = $_POST["confirm-password"];
+
+        if(trim($email) != ""){
+            echo "USER email is NOT EMPTY<br>". $email;
+        } else{
+            echo "field email is empty<br>";
+        } 
+
+        if(trim($username) != ""){
+            echo "USER username is NOT EMPTY<br>". $username;
+        } else{
+            echo "field username is empty<br>";
+        } 
+
+        if(trim($password) != ""){
+            echo "USER password is NOT EMPTY<br>". $password;
+        } else{
+            echo "field password is empty<br>";
+        } 
+
+        if(trim($confirm_password) != ""){
+            echo "USER confirm_password is NOT EMPTY<br>". $confirm_password;
+        } else{
+            echo "field confirm_password is empty<br>";
+        } 
+
+    }
+
+?>
